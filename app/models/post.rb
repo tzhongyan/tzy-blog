5dfa6a0a308d2201530a5baeb1b23d000a1095a9
Tan Zhong Yan <iam@tzhongyan.com>
2017-11-08 16:39:59 +0800

    Post and categories

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,15 @@
 class Post < ApplicationRecord
-    belongs_to :category
-    validates :title, :content, :category_id, presence: true
+  belongs_to :category
+  validates :title, :content, :category_id, presence: true
+
+  def index
+      
+    if params[:category].blank?
+      @posts = Post.all.order("created_at DESC")
+    
+    else
+        @category_id = Category.find_by(name: params[:category]).id
+        @posts = Post.where(category_id: @category_id).order("created_at DESC")
+    end
+  end
 end
