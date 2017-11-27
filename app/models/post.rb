993cfdb965da55b1f1f6ac8833081da5e6b5ed8b
Tan Zhong Yan <iam@tzhongyan>
2017-11-28 05:36:35 +0800

    implementing disqus comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,10 +4,10 @@ class Post < ApplicationRecord
 
   def index
     if params[:category].blank?
-      @posts = Post.all.order("created_at DESC")
+      @posts = Post.all.order("id DESC")
     else
       @category_id = Category.find_by(name: params[:category]).id
-      @posts = Post.where(category_id: @category_id).order("created_at DESC")
+      @posts = Post.where(category_id: @category_id).order("id DESC")
     end
   end
 end
